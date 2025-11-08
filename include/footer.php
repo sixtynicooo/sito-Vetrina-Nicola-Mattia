df14b29c9c09390996128e401ed0e33c75e4e862
sixtynicooo <sixtynicooo@gmail.com>
2025-11-08 12:01:58 +0100

    Update: aggiunta pagina politica cookye, data ann dinamica

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,16 @@
 <footer class="site-footer">
     <div class="footer-content">
-        <p>&copy; 2024 Il Mio Sito. Tutti i diritti riservati.</p>
+        <p id="copyright"></p>
         <a href="../pagine/politica-cookie.php">Politica cookie</a>
     </div>
 </footer>
 </body>
 
 </html>
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const year = new Date().getFullYear();
+        document.getElementById("copyright").textContent =
+            `© ${year} Tutti i diritti riservati`;
+});
+</script>
