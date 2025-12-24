ca006d75f9f1142516f96656f4d5fcb90a12ff37
sixtynicooo <sixtynicooo@gmail.com>
2025-12-24 11:07:32 +0100

    Fix ora 2 bottoni non possono essere in 2 righe distinte

--- include/banner-cookie.php
+++ include/banner-cookie.php
@@ -2,6 +2,9 @@
     Utilizziamo cookie tecnici e, previo consenso, cookie di terze parti per migliorare l’esperienza di navigazione.
     Puoi accettare, rifiutare o personalizzare le tue preferenze.
     Per maggiori informazioni consulta la nostra <a href="./politica-cookie.php"> Cookie Policy</a>.
-    <button id="banner-accept-all" class="accept-btn" style="margin-left: 10px; padding: 8px 12px; border: none; cursor: pointer; background: #4CAF50; color: white;">Accetta</button>
-    <button id="banner-reject-all" class="decline-btn"  style="margin-left: 10px; padding: 8px 12px; border: none; cursor: pointer; background: #d9534f; color: white;">Rifiuta</button>
+    <div style="display: block;">
+        <button id="banner-accept-all" class="accept-btn" style="margin-left: 10px; padding: 8px 12px; border: none; cursor: pointer; background: #4CAF50; color: white;">Accetta</button>
+        <button id="banner-reject-all" class="decline-btn"  style="margin-left: 10px; padding: 8px 12px; border: none; cursor: pointer; background: #d9534f; color: white;">Rifiuta</button>
+    </div>
+    
 </div>
